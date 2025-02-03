11e782f7afd76c37969a092362b8e4509d6ed24d
shhbyar <shady.the.dev@gmail.com>
2025-02-03 20:25:48 +0330

    from windows

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Lusitana, Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusi = Lusitana({ subsets: ["latin"], weight: ["400", "700"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700"] });
